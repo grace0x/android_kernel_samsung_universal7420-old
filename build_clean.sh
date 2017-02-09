fa1d90f812e3c9c332f0ca1f9ab58c50352a0326
G-Joker <g-joker@foxmail.com>
2017-02-09 13:12:09 +0000

    Script: update build_clean.sh

--- build_clean.sh
+++ build_clean.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # For clean Kerneldir
+BUILD_WHERE=$(pwd)
+BUILD_KERNEL_DIR=$BUILD_WHERE
+BUILD_CROSS_COMPILE=/home/aarch64-sabermod-7.0/bin/aarch64-
+
 make clean
 make distclean
 make mrproper
+
+rm -rf $BUILD_KERNEL_DIR/fips_fmp_utils
+rm -rf $BUILD_KERNEL_DIR/fmp_hmac.bin
